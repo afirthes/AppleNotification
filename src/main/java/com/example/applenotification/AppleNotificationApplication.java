bace47a6889cc54f659720a33888328373b36ccd
Sergio <afirthes@gmail.com>
2023-02-16 17:55:11 +0300

    Initial commit

---
+++ src/main/java/com/example/applenotification/AppleNotificationApplication.java
@@ -0,0 +1,81 @@
+package com.example.applenotification;
+
+import com.eatthepath.pushy.apns.ApnsClient;
+import com.eatthepath.pushy.apns.ApnsClientBuilder;
+import com.eatthepath.pushy.apns.auth.ApnsSigningKey;
+import com.eatthepath.pushy.apns.util.ApnsPayloadBuilder;
+import com.eatthepath.pushy.apns.util.SimpleApnsPayloadBuilder;
+import com.eatthepath.pushy.apns.util.SimpleApnsPushNotification;
+import com.eatthepath.pushy.apns.util.TokenUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+import java.util.concurrent.Future;
+
+@SpringBootApplication
+public class AppleNotificationApplication {
+
+    @Value("${teamId}")
+    private String teamId;
+
+    @Value("${keyPassword}")
+    private String keyPassword;
+
+    @Value("${p8FilePath}")
+    private String p8FilePath;
+
+    @Value("${deviceToken}")
+    private String deviceToken;
+
+    @Value("${appBundleId}")
+    private String appBundleId;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleNotificationApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+
+            System.out.println("Sending...");
+
+            final ApnsClient apnsClient = new ApnsClientBuilder()
+                    .setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
+                    .setSigningKey(ApnsSigningKey.loadFromPkcs8File(new File(p8FilePath),
+                            teamId, keyPassword))
+                    .build();
+
+
+            final SimpleApnsPushNotification pushNotification;
+
+
+            final ApnsPayloadBuilder payloadBuilder = new SimpleApnsPayloadBuilder();
+            payloadBuilder.setAlertTitle("Alert!")
+                    .setAlertBody("Alert body!");
+
+
+            final String payload = payloadBuilder.build();
+            final String token = TokenUtil.sanitizeTokenString(deviceToken);
+
+            pushNotification = new SimpleApnsPushNotification(token, appBundleId, payload);
+
+            var future = apnsClient.sendNotification(pushNotification);
+
+
+            var response = future.get();
+
+            System.out.println("End");
+
+            apnsClient.close();
+
+
+        };
+    }
+
+}
---
+++ src/test/java/com/example/applenotification/AppleNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applenotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
